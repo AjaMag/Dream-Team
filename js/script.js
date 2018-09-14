0f89bb08ed2bbaf4ed724936546f9934137c4703
Voriah <rodgick@yahoo.com>
2018-09-13 20:56:23 -0700

    searchychy

--- js/script.js
+++ js/script.js
@@ -18,10 +18,11 @@ function showPosition(position) {
   lat = position.coords.latitude;
   long = position.coords.longitude;
   console.log(lat, long)
-  var hikingURL = `https://www.hikingproject.com/data/get-trails?maxResults=20&lat=${lat}&lon=${long}&maxDistance=5&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
+  var hikingURL = `https://www.hikingproject.com/data/get-trails?maxResults=20&lat=${lat}&lon=${long}&maxDistance=10&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
   
    $.get(hikingURL)
    .then(function(response) {
+     $("#cardholder").empty();
     for (i = 0; i < response.trails.length; i++) {
     $("#cardholder").append(`
     <a class="carousel-item">  
@@ -55,7 +56,6 @@ $(document).on("click", ".addFavorite", function () {
   favs.push($(this).attr("data-id"))
 })
 
-getLocation()
 
   favs.on("child_added", function (favorites) {
     var hikingURL = `https://www.hikingproject.com/data/get-trails-by-id?ids=${favorites.val()}&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
@@ -91,8 +91,57 @@ getLocation()
 })
 
 function findLocation () {
-  var local;
+  event.preventDefault();
+  
+  var local = $("#cityName").val();
   $.get(`http://dev.virtualearth.net/REST/v1/Locations/${local}?&maxResults=1&key=Ami0rQuZG9aaTceHF0XA2qTY0BWc1D5gUXmI0R1VJ_URY8sHjBb4ksEK85edNRjY`)
+  .then(function (localResponse) {
+    console.log(localResponse);
+    lat = localResponse.resourceSets[0].resources[0].point.coordinates[0];
+    long = localResponse.resourceSets[0].resources[0].point.coordinates[1];
+    var hikingURL = `https://www.hikingproject.com/data/get-trails?maxResults=20&lat=${lat}&lon=${long}&maxDistance=10&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
+    
+    $.get(hikingURL)
+    .then(function (response) {
+      $("#cardholder").empty();
+        for (i = 0; i < response.trails.length; i++) {
+          $("#cardholder").append(`
+    <a class="carousel-item">  
+    <div class="card">
+      <div class="card-image">
+        <img src="${response.trails[i].imgSmallMed}">
+        <span class="card-title">${response.trails[i].name}</span>
+      </div>
+      <div class="card-content">
+        <p>${response.trails[i].location}</p>
+        <br>
+        <p>${response.trails[i].summary}</p>
+      </div>
+       <div class="card-action">
+       <p class="addFavorite" data-id="${response.trails[i].id}">Add to Favorites</p>
+        </div>
+    </div>
+    </a>
+    `)
+        }
+        setTimeout(function () {
+          $('.carousel').carousel();
+          $('.carousel-slider').slider({ full_width: true });
+        }, 500)
+        console.log(response)
+      })
+  })
+
+
+
+
+
+
+
+
+
+
+
 }
 //Weather API call
 function weatherAPI (position) {
@@ -110,7 +159,7 @@ function weatherAPI (position) {
   })
 }
 
-getLocation()
-setTimeout(function (){
-  weatherAPI()
-},4000)
+// getLocation()
+// setTimeout(function (){
+//   weatherAPI()
+// },4000)
