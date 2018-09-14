3dc1fbaa3b2fc544b4769ef28065e3359446dfb8
Voriah <rodgick@yahoo.com>
2018-09-13 20:29:43 -0700

    other locationsstuffs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var lat;
 var long;
+var db = firebase.database();
+var favs = db.ref("favorites");
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -11,7 +13,7 @@ function showPosition(position) {
   lat = position.coords.latitude;
   long = position.coords.longitude;
   console.log(lat, long)
-  var hikingURL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=10&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
+  var hikingURL = `https://www.hikingproject.com/data/get-trails?maxResults=20&lat=${lat}&lon=${long}&maxDistance=5&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
   
    $.get(hikingURL)
    .then(function(response) {
@@ -28,6 +30,9 @@ function showPosition(position) {
         <br>
         <p>${response.trails[i].summary}</p>
       </div>
+       <div class="card-action">
+       <p class="addFavorite" data-id="${response.trails[i].id}">Add to Favorites</p>
+        </div>
     </div>
     </a>
     `)
@@ -40,4 +45,47 @@ function showPosition(position) {
    })
 }
 
+$(document).on("click", ".addFavorite", function () {
+
+  favs.push($(this).attr("data-id"))
+})
+
 getLocation()
+
+  favs.on("child_added", function (favorites) {
+    var hikingURL = `https://www.hikingproject.com/data/get-trails-by-id?ids=${favorites.val()}&key=200356178-455274bda6e2c8c2496858d99e90dcc7`;
+
+    $.get(hikingURL)
+      .then(function (response) {
+
+        console.log(response)
+          $("#favholder").append(`
+    <a class="carousel-item">  
+    <div class="card">
+      <div class="card-image">
+        <img src="${response.trails[0].imgSmallMed}">
+        <span class="card-title">${response.trails[0].name}</span>
+      </div>
+      <div class="card-content">
+        <p>${response.trails[0].location}</p>
+        <br>
+        <p>${response.trails[0].summary}</p>
+      </div>
+       <div class="card-action">
+       <p data-id="${response.trails.id}">Favorite!</p>
+        </div>
+    </div>
+    </a>
+    `)
+  })
+
+    setTimeout(function () {
+      $('.carousel').carousel();
+      $('.carousel-slider').slider({ full_width: true });
+    }, 500)
+})
+
+function findLocation () {
+  var local;
+  $.get(`http://dev.virtualearth.net/REST/v1/Locations/${local}?&maxResults=1&key=Ami0rQuZG9aaTceHF0XA2qTY0BWc1D5gUXmI0R1VJ_URY8sHjBb4ksEK85edNRjY`)
+}
